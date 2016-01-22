29001fbfd48f33ebee507a756d2b2465a4b89bb6
Tamás Kert <tamas-kert@hotmail.de>
2016-01-22 12:35:02 +0100

    4.5  + button

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.zahl1Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // zahl1Button
             // 
-            this.button1.Location = new System.Drawing.Point(119, 99);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.zahl1Button.Location = new System.Drawing.Point(119, 99);
+            this.zahl1Button.Name = "zahl1Button";
+            this.zahl1Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl1Button.TabIndex = 0;
+            this.zahl1Button.Text = "1";
+            this.zahl1Button.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zahl1Button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button zahl1Button;
     }
 }
 
