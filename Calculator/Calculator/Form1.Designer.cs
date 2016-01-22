a48ac8ef84865a4ecdcf113841dd9003e1ab24e0
Tamás Kert <tamas-kert@hotmail.de>
2016-01-22 12:41:38 +0100

    Zahlen von 0-9

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -29,22 +29,121 @@
         private void InitializeComponent()
         {
             this.zahl1Button = new System.Windows.Forms.Button();
+            this.zahl2Button = new System.Windows.Forms.Button();
+            this.zahl3Button = new System.Windows.Forms.Button();
+            this.zahl4Button = new System.Windows.Forms.Button();
+            this.zahl5Button = new System.Windows.Forms.Button();
+            this.zahl6Button = new System.Windows.Forms.Button();
+            this.zahl9Button = new System.Windows.Forms.Button();
+            this.zahl8Button = new System.Windows.Forms.Button();
+            this.zahl7Button = new System.Windows.Forms.Button();
+            this.zahl0Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // zahl1Button
             // 
-            this.zahl1Button.Location = new System.Drawing.Point(119, 99);
+            this.zahl1Button.Location = new System.Drawing.Point(43, 78);
             this.zahl1Button.Name = "zahl1Button";
             this.zahl1Button.Size = new System.Drawing.Size(30, 29);
             this.zahl1Button.TabIndex = 0;
             this.zahl1Button.Text = "1";
             this.zahl1Button.UseVisualStyleBackColor = true;
             // 
+            // zahl2Button
+            // 
+            this.zahl2Button.Location = new System.Drawing.Point(98, 78);
+            this.zahl2Button.Name = "zahl2Button";
+            this.zahl2Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl2Button.TabIndex = 1;
+            this.zahl2Button.Text = "2";
+            this.zahl2Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl3Button
+            // 
+            this.zahl3Button.Location = new System.Drawing.Point(151, 78);
+            this.zahl3Button.Name = "zahl3Button";
+            this.zahl3Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl3Button.TabIndex = 2;
+            this.zahl3Button.Text = "3";
+            this.zahl3Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl4Button
+            // 
+            this.zahl4Button.Location = new System.Drawing.Point(43, 122);
+            this.zahl4Button.Name = "zahl4Button";
+            this.zahl4Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl4Button.TabIndex = 3;
+            this.zahl4Button.Text = "4";
+            this.zahl4Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl5Button
+            // 
+            this.zahl5Button.Location = new System.Drawing.Point(98, 122);
+            this.zahl5Button.Name = "zahl5Button";
+            this.zahl5Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl5Button.TabIndex = 4;
+            this.zahl5Button.Text = "5";
+            this.zahl5Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl6Button
+            // 
+            this.zahl6Button.Location = new System.Drawing.Point(151, 122);
+            this.zahl6Button.Name = "zahl6Button";
+            this.zahl6Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl6Button.TabIndex = 5;
+            this.zahl6Button.Text = "6";
+            this.zahl6Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl9Button
+            // 
+            this.zahl9Button.Location = new System.Drawing.Point(151, 170);
+            this.zahl9Button.Name = "zahl9Button";
+            this.zahl9Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl9Button.TabIndex = 6;
+            this.zahl9Button.Text = "9";
+            this.zahl9Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl8Button
+            // 
+            this.zahl8Button.Location = new System.Drawing.Point(98, 170);
+            this.zahl8Button.Name = "zahl8Button";
+            this.zahl8Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl8Button.TabIndex = 7;
+            this.zahl8Button.Text = "8";
+            this.zahl8Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl7Button
+            // 
+            this.zahl7Button.Location = new System.Drawing.Point(43, 170);
+            this.zahl7Button.Name = "zahl7Button";
+            this.zahl7Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl7Button.TabIndex = 8;
+            this.zahl7Button.Text = "7";
+            this.zahl7Button.UseVisualStyleBackColor = true;
+            // 
+            // zahl0Button
+            // 
+            this.zahl0Button.Location = new System.Drawing.Point(98, 220);
+            this.zahl0Button.Name = "zahl0Button";
+            this.zahl0Button.Size = new System.Drawing.Size(30, 29);
+            this.zahl0Button.TabIndex = 9;
+            this.zahl0Button.Text = "0";
+            this.zahl0Button.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.zahl0Button);
+            this.Controls.Add(this.zahl7Button);
+            this.Controls.Add(this.zahl8Button);
+            this.Controls.Add(this.zahl9Button);
+            this.Controls.Add(this.zahl6Button);
+            this.Controls.Add(this.zahl5Button);
+            this.Controls.Add(this.zahl4Button);
+            this.Controls.Add(this.zahl3Button);
+            this.Controls.Add(this.zahl2Button);
             this.Controls.Add(this.zahl1Button);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +154,15 @@
         #endregion
 
         private System.Windows.Forms.Button zahl1Button;
+        private System.Windows.Forms.Button zahl2Button;
+        private System.Windows.Forms.Button zahl3Button;
+        private System.Windows.Forms.Button zahl4Button;
+        private System.Windows.Forms.Button zahl5Button;
+        private System.Windows.Forms.Button zahl6Button;
+        private System.Windows.Forms.Button zahl9Button;
+        private System.Windows.Forms.Button zahl8Button;
+        private System.Windows.Forms.Button zahl7Button;
+        private System.Windows.Forms.Button zahl0Button;
     }
 }
 
