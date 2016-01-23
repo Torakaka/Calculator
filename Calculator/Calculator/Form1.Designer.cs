234d7467ec2eecead44074f8d0c1f7392ad385eb
Tamás Kert <tamas-kert@hotmail.de>
2016-01-23 18:40:56 +0100

    Anzeige Klappt.

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.zahl7Button = new System.Windows.Forms.Button();
             this.zahl0Button = new System.Windows.Forms.Button();
             this.AnzeigeTextBox = new System.Windows.Forms.TextBox();
+            this.rechenoperatorMinusButton = new System.Windows.Forms.Button();
+            this.rechenoperatorPlusButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // zahl1Button
@@ -49,6 +51,7 @@
             this.zahl1Button.TabIndex = 0;
             this.zahl1Button.Text = "1";
             this.zahl1Button.UseVisualStyleBackColor = true;
+            this.zahl1Button.Click += new System.EventHandler(this.zahl1Button_Click);
             // 
             // zahl2Button
             // 
@@ -58,6 +61,7 @@
             this.zahl2Button.TabIndex = 1;
             this.zahl2Button.Text = "2";
             this.zahl2Button.UseVisualStyleBackColor = true;
+            this.zahl2Button.Click += new System.EventHandler(this.zahl2Button_Click);
             // 
             // zahl3Button
             // 
@@ -67,6 +71,7 @@
             this.zahl3Button.TabIndex = 2;
             this.zahl3Button.Text = "3";
             this.zahl3Button.UseVisualStyleBackColor = true;
+            this.zahl3Button.Click += new System.EventHandler(this.zahl3Button_Click);
             // 
             // zahl4Button
             // 
@@ -76,6 +81,7 @@
             this.zahl4Button.TabIndex = 3;
             this.zahl4Button.Text = "4";
             this.zahl4Button.UseVisualStyleBackColor = true;
+            this.zahl4Button.Click += new System.EventHandler(this.zahl4Button_Click);
             // 
             // zahl5Button
             // 
@@ -85,6 +91,7 @@
             this.zahl5Button.TabIndex = 4;
             this.zahl5Button.Text = "5";
             this.zahl5Button.UseVisualStyleBackColor = true;
+            this.zahl5Button.Click += new System.EventHandler(this.zahl5Button_Click);
             // 
             // zahl6Button
             // 
@@ -94,6 +101,7 @@
             this.zahl6Button.TabIndex = 5;
             this.zahl6Button.Text = "6";
             this.zahl6Button.UseVisualStyleBackColor = true;
+            this.zahl6Button.Click += new System.EventHandler(this.zahl6Button_Click);
             // 
             // zahl9Button
             // 
@@ -103,6 +111,7 @@
             this.zahl9Button.TabIndex = 6;
             this.zahl9Button.Text = "9";
             this.zahl9Button.UseVisualStyleBackColor = true;
+            this.zahl9Button.Click += new System.EventHandler(this.zahl9Button_Click);
             // 
             // zahl8Button
             // 
@@ -112,6 +121,7 @@
             this.zahl8Button.TabIndex = 7;
             this.zahl8Button.Text = "8";
             this.zahl8Button.UseVisualStyleBackColor = true;
+            this.zahl8Button.Click += new System.EventHandler(this.zahl8Button_Click);
             // 
             // zahl7Button
             // 
@@ -121,6 +131,7 @@
             this.zahl7Button.TabIndex = 8;
             this.zahl7Button.Text = "7";
             this.zahl7Button.UseVisualStyleBackColor = true;
+            this.zahl7Button.Click += new System.EventHandler(this.zahl7Button_Click);
             // 
             // zahl0Button
             // 
@@ -130,6 +141,7 @@
             this.zahl0Button.TabIndex = 9;
             this.zahl0Button.Text = "0";
             this.zahl0Button.UseVisualStyleBackColor = true;
+            this.zahl0Button.Click += new System.EventHandler(this.zahl0Button_Click);
             // 
             // AnzeigeTextBox
             // 
@@ -139,11 +151,31 @@
             this.AnzeigeTextBox.Size = new System.Drawing.Size(138, 20);
             this.AnzeigeTextBox.TabIndex = 10;
             // 
+            // rechenoperatorMinusButton
+            // 
+            this.rechenoperatorMinusButton.Location = new System.Drawing.Point(198, 78);
+            this.rechenoperatorMinusButton.Name = "rechenoperatorMinusButton";
+            this.rechenoperatorMinusButton.Size = new System.Drawing.Size(30, 29);
+            this.rechenoperatorMinusButton.TabIndex = 11;
+            this.rechenoperatorMinusButton.Text = "-";
+            this.rechenoperatorMinusButton.UseVisualStyleBackColor = true;
+            // 
+            // rechenoperatorPlusButton
+            // 
+            this.rechenoperatorPlusButton.Location = new System.Drawing.Point(198, 122);
+            this.rechenoperatorPlusButton.Name = "rechenoperatorPlusButton";
+            this.rechenoperatorPlusButton.Size = new System.Drawing.Size(30, 29);
+            this.rechenoperatorPlusButton.TabIndex = 12;
+            this.rechenoperatorPlusButton.Text = "+";
+            this.rechenoperatorPlusButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.rechenoperatorPlusButton);
+            this.Controls.Add(this.rechenoperatorMinusButton);
             this.Controls.Add(this.AnzeigeTextBox);
             this.Controls.Add(this.zahl0Button);
             this.Controls.Add(this.zahl7Button);
@@ -175,6 +207,8 @@
         private System.Windows.Forms.Button zahl7Button;
         private System.Windows.Forms.Button zahl0Button;
         private System.Windows.Forms.TextBox AnzeigeTextBox;
+        private System.Windows.Forms.Button rechenoperatorMinusButton;
+        private System.Windows.Forms.Button rechenoperatorPlusButton;
     }
 }
 
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -16,5 +16,69 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        string Anzeige;
+
+        double [] zahl = new double[100];
+
+        private void zahl1Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(1);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl2Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(2);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl3Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(3);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl4Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(4);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl5Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(5);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl6Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(6);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl7Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(7);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl8Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(8);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl9Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(9);
+            AnzeigeTextBox.Text = Anzeige;
+        }
+
+        private void zahl0Button_Click(object sender, EventArgs e)
+        {
+            Anzeige = Anzeige + Convert.ToString(0);
+            AnzeigeTextBox.Text = Anzeige;
+        }
     }
 }
