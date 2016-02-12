f4580c2b0ab6b0c74b5859c730aa42d0b4ac0c2f
ManU8991 <iah42dettori@gso-koeln.de>
2016-02-12 11:46:10 +0100

    GB gespart

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -24,140 +24,60 @@ namespace Calculator
 
         private void zahl1Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[4] = 1;
-                Anzeige = Convert.ToString(zahl[4]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(1);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl2Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[5] = 2;
-                Anzeige = Convert.ToString(zahl[5]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(2);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl3Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[6] = 3;
-                Anzeige = Convert.ToString(zahl[6]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(3);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl4Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[7] = 4;
-                Anzeige = Convert.ToString(zahl[7]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(4);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl5Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[8] = 5;
-                Anzeige = Convert.ToString(zahl[8]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(5);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl6Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[9] = 6;
-                Anzeige = Convert.ToString(zahl[9]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(6);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl7Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[10] = 7;
-                Anzeige = Convert.ToString(zahl[10]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(7);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl8Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[11] = 8;
-                Anzeige = Convert.ToString(zahl[11]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(8);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl9Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[12] = 9;
-                Anzeige = Convert.ToString(zahl[12]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(9);
             AnzeigeTextBox.Text = Anzeige;
         }
 
         private void zahl0Button_Click(object sender, EventArgs e)
         {
-            if (prüfer == true)
-            {
-                zahl[12] = 0;
-                Anzeige = Convert.ToString(zahl[12]);
-                AnzeigeTextBox.Text = Anzeige;
-                prüfer = false;
-            }
-            else
                 Anzeige = Anzeige + Convert.ToString(0);
             AnzeigeTextBox.Text = Anzeige;
         }
@@ -242,13 +162,12 @@ namespace Calculator
         {
             if (prüfer == true)
             {
-                
                 Anzeige = "0,";
                 AnzeigeTextBox.Text = Anzeige;
                 prüfer = false;
             }
             else
-                Anzeige = Anzeige +",";
+                Anzeige = Anzeige + ",";
             AnzeigeTextBox.Text = Anzeige;
         }
     }
